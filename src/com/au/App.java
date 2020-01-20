424d4c3bc5672864e3a6f10ab28119e475f9779b
Saumyadeep Tarafdar <saumyadeep.tarafdar@accoliteindia.com>
2020-01-20 10:01:16 +0530

    Anonymous Inner Class for Runnable and lambda expression

--- src/com/au/App.java
+++ src/com/au/App.java
@@ -4,9 +4,18 @@ public class App {
 
 	public static void main(String[] args) {
 
-		Thread worker = new Thread(new Worker2());
+		Thread worker = new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				System.out.println("Inner Says Hi!");
+			}
+
+		});
 		worker.start();
 
-
+		Thread worker2 = new Thread(() -> System.out.println("Inner Says Hi!"));
+		worker2.start();
+		
 	}
 }
